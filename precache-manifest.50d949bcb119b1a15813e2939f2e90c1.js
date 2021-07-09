de3c442d3c2956222e7ce3e32a7f8d05b734a9e7
Sude ALAÇATI <deiseu@outlook.com.tr>
2021-07-09 17:42:48 +0200

    Add files via upload

---
+++ precache-manifest.50d949bcb119b1a15813e2939f2e90c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6338c4b9d8a439a38d5cee239386fd",
+    "url": "/serendia-status/index.html"
+  },
+  {
+    "revision": "68c92b5ba1f68d5d33db",
+    "url": "/serendia-status/static/css/main.b427710c.chunk.css"
+  },
+  {
+    "revision": "7ccfbfba1b5c809aab4b",
+    "url": "/serendia-status/static/js/2.e2a633d6.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/serendia-status/static/js/2.e2a633d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c92b5ba1f68d5d33db",
+    "url": "/serendia-status/static/js/main.d3e7c4ee.chunk.js"
+  },
+  {
+    "revision": "b5b3a6d90bd254cae9fb",
+    "url": "/serendia-status/static/js/runtime-main.98ed2461.js"
+  }
+]);
